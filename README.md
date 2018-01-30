393cca2fdbed3d084ea74747ee299491f9197f22
Josue Vargas Vasquez <jos.vargasv@gmail.com>
2018-01-29 21:16:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Form Inline Edit using backend validation
+
+[![Latest Version on NPM](https://img.shields.io/npm/v/:package_name.svg?style=flat-square)](https://npmjs.com/package/:package_name)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
+
+TODO: descripction
+
+## Install
+
+You can install the package via yarn:
+
+```bash
+$ yarn add form-inline-edit
+```
+
+## Usage
+
+```js
+const FormInlineEdit = require('form-inline-edit');
+
+const form = new FormInlineEdit({
+    name: 'josh',
+    age: 100
+})
+```
+
+### Testing (TODO)
+
+``` bash
+$ npm run test
+```
+
+### Security
+
+If you discover any security related issues, please contact :author_name instead of using the issue tracker.
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+## Credits
+
+- [Josue Vargas](https://github.com/josvar)
+- [All Contributors](../../contributors)
---
+++ src/Form.js
@@ -0,0 +1,48 @@
+const SpatieForm = require('form-backend-validation')
+const cloneDeep = require('clone-deep')
+
+class Form {
+  constructor(data = {}, options = {}) {
+    this.editing = false
+    this.spatieForm = new SpatieForm({}, options)
+    this.form = data
+    this.presentation = data
+  }
+
+  edit() {
+    this.editing = true
+  }
+
+  errors() {
+    return this.spatieForm.errors
+  }
+
+  cancel() {
+    this.form = cloneDeep(this.presentation)
+
+    this.editing = false
+  }
+
+  async submit({ only, transformers = {} }) {
+    // TODO: add support to only option
+
+    let data = Object.keys(this.form).reduce((carry, key) => {
+      carry[key] = transformers[key]
+        ? transformers[key](this.form[key])
+        : this.form[key]
+
+      return carry
+    }, {})
+
+    this.spatieForm.populate(data)
+
+    let response = await this.spatieForm[this.method]
+    this.presentation = cloneDeep(this.form)
+    this.spatieForm.reset()
+    this.editing = false
+
+    return response
+  }
+}
+
+export default Form
